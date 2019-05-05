44a845242b54f5d77d7c17b678efde66ecab994c
ChenSheng <781159385@qq.com>
2019-05-05 18:53:32 +0800

    add Time complexity

--- 03-Nonlinear_structure/BSTSet.java
+++ 03-Nonlinear_structure/BSTSet.java
@@ -1,12 +1,13 @@
 package com.geektech.Nonlinear_structure;
 import com.geektech.Semilinear_structure.BST;
 /**
+ *  基于搜索树实现的Set，都是有序集合
  *  基于BST实现的Set,属于更加高级的数据结构  date = 4/5 2019 ,  author = chensheng
- *  ADT: 构造器：constructor1个
- *        基本：getSize, isEmpty
- *        增加：add(e),不能有重复元素
- *        查询：contains(e)
- *        删除：remove(e)
+ *  ADT: 构造器：constructor1个 => O(1)
+ *        基本：getSize, isEmpty => O(1)
+ *        增加：add(e),不能有重复元素  => O(h): 平均时间性能O(logn) ， 最差退化成链表 O(n) 。 n为节点数
+ *        查询：contains(e)  => O(h)
+ *        删除：remove(e)  => O(h)
  **/
 public class BSTSet<E extends Comparable<E>> implements Set<E>{
 
--- 03-Nonlinear_structure/LinkedListSet.java
+++ 03-Nonlinear_structure/LinkedListSet.java
@@ -3,13 +3,15 @@ package com.geektech.Nonlinear_structure;
 import com.geektech.linear_structure.base.LinkedList;
 
 /**
+ *  基于链表或者哈希表实现的Set，都是无序集合
  *  基于LinkedList实现的Set,属于更加高级的数据结构  date = 5/5 2019 ,  author = chensheng
- *  ADT: 构造器：constructor1个
- *        基本：getSize, isEmpty
- *        增加：add(e),不能有重复元素
- *        查询：contains(e)
- *        删除：remove(e)
+ *  ADT: 构造器：constructor1个 => O(1)
+ *        基本：getSize, isEmpty  => O(1)
+ *        增加：add(e),不能有重复元素 => O(n) + O(1) = O(n)
+ *        查询：contains(e)  => O(n)
+ *        删除：remove(e)   => O(n)
  **/
+
 public class LinkedListSet<E> implements Set<E> {
 
     private LinkedList<E> list;
@@ -20,7 +22,7 @@ public class LinkedListSet<E> implements Set<E> {
 
     @Override
     public void add(E e) { //不能有重复的元素
-        if(!list.contains(e))
+        if(!list.contains(e))  // O(n)
             list.addFirst(e);  //头添加，O(1)
     }
 
